5632e758572287e334c602e7b271d5da84f659a1
talarisulochana <talarisulochana123@gmail.com>
2025-05-06 10:50:44 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,6 @@
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Thank you for contacting us! We'll get back to you soon.");
+    this.reset();
+  });
+  
